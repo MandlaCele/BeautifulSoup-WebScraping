980e6cc0d322751aa826be220c0662b512feea38
sihle <141397524+MandlaCele@users.noreply.github.com>
2026-08-01 10:56:07 +0200

    Refactor scrape.py for better structure and readability
    Refactored the main function to improve structure and readability, ensuring the response is checked after each request.

--- scrape.py
+++ scrape.py
@@ -1,26 +1,35 @@
+#!/usr/bin/env python3
+
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://books.toscrape.com/"
 
-response = requests.get(url)
+def main():
 
-if response.status_code == 200:
+    url = "https://books.toscrape.com/"
 
-    soup = BeautifulSoup(response.text, "html.parser")
+    response = requests.get(url)
 
-    books = soup.find_all("article", class_="product_pod")
+    if response.status_code == 200:
 
-    print("Top Books Available")
-    print("-------------------")
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    for book in books[:10]:
-        title = book.h3.a["title"]
-        price = book.find("p", class_="price_color").text
+        books = soup.find_all("article", class_="product_pod")
 
-        print("Book:", title)
-        print("Price:", price)
+        print("Top Books Available")
         print("-------------------")
 
-else:
-    print("Unable to access website")
+        for book in books[:10]:
+            title = book.h3.a["title"]
+            price = book.find("p", class_="price_color").text
+
+            print("Book:", title)
+            print("Price:", price)
+            print("-------------------")
+
+    else:
+        print("Unable to access website")
+
+
+if __name__ == "__main__":
+    main()
