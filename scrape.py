306ad7168da54d13b6e3e793a8ac7a7b99e82a56
sihle <141397524+MandlaCele@users.noreply.github.com>
2026-08-01 10:50:18 +0200

    Add web scraping functionality for top books
    This script scrapes the top 10 books and their prices from the specified URL.

---
+++ scrape.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://books.toscrape.com/"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    books = soup.find_all("article", class_="product_pod")
+
+    print("Top Books Available")
+    print("-------------------")
+
+    for book in books[:10]:
+        title = book.h3.a["title"]
+        price = book.find("p", class_="price_color").text
+
+        print("Book:", title)
+        print("Price:", price)
+        print("-------------------")
+
+else:
+    print("Unable to access website")
